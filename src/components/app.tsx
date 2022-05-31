7a018466f41d7b1ed1aa1413e6c17e4035931cdc
Zemian Deng <zemian.deng@oracle.com>
2022-05-31 09:55:11 -0400

    Add JET router navigation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,6 +4,8 @@ import Context = require("ojs/ojcontext");
 import {Footer} from "./footer";
 import {Header} from "./header";
 import {Content} from "./content/index";
+import CoreRouter = require("ojs/ojcorerouter");
+import UrlParamAdapter = require("ojs/ojurlparamadapter");
 
 type Props = {
     appName?: string;
@@ -17,6 +19,53 @@ export class App extends Component<ExtendGlobalProps<Props>> {
         userLogin: "john.hancock@oracle.com"
     };
 
+    routes = [
+        { path: "", redirect: "counter" },
+        {
+            path: "counter",
+            detail: {
+                label: "Counter",
+                iconClass: "oj-navigationlist-item-icon oj-ux-ico-binding-control"
+            }
+        },
+        {
+            path: "flightbooker",
+            detail: {
+                label: "Flight Booker",
+                iconClass: "oj-navigationlist-item-icon oj-ux-ico-binding-control"
+            }
+        },
+        {
+            path: "tempconverter",
+            detail: {
+                label: "Tempature Converter",
+                iconClass: "oj-navigationlist-item-icon oj-ux-ico-binding-control"
+            }
+        }
+    ];
+
+    router = new CoreRouter<CoreRouter.DetailedRouteConfig>(this.routes, {
+        urlAdapter: new UrlParamAdapter()
+    });
+
+    state = { currentRoutePath: "" };
+
+    constructor(props: ExtendGlobalProps<Props>) {
+        super(props);
+        this.router.currentState.subscribe(this.onRouteChanged.bind(this));
+        this.router.sync();
+    }
+
+    onRouteChanged(actionable: CoreRouter.ActionableState<CoreRouter.DetailedRouteConfig>): void {
+        // Update our state based on new router state
+        const routePath = actionable.state?.path;
+        this.setState({ currentRoutePath: routePath });
+    }
+
+    changeRoute = (routePath: string) => {
+        this.router.go({ path: routePath });
+    };
+
     render(props: ExtendGlobalProps<Props>): ComponentChild {
         return (
             <div id="appContainer" class="oj-web-applayout-page">
@@ -24,7 +73,7 @@ export class App extends Component<ExtendGlobalProps<Props>> {
                     appName={props.appName}
                     userLogin={props.userLogin}
                 />
-                <Content/>
+                <Content page={this.state.currentRoutePath} changeRoute={this.changeRoute} routes={this.routes}/>
                 <Footer/>
             </div>
         );
--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,11 +1,58 @@
-import { h } from "preact";
+import {h} from "preact";
+import {useMemo} from "preact/hooks";
 
+import {Counter} from "../gui/Counter";
 import {FlightBooker} from "../gui/FlightBooker";
+import {TemperatureConverter} from "../gui/TemperatureConverter";
 
-export function Content() {
-  return (
-    <div class="oj-web-applayout-max-width oj-web-applayout-content">
-      <FlightBooker></FlightBooker>
-    </div>
-  );
+import "ojs/ojnavigationlist";
+import ArrayDataProvider = require("ojs/ojarraydataprovider");
+import {ojNavigationList} from "ojs/ojnavigationlist";
+import {Route} from "ojs/ojcorerouter";
+
+export function Content(props) {
+    const routesDP = useMemo(() =>
+        new ArrayDataProvider(props.routes.slice(1), {keyAttributes: "path"}), []);
+
+    const navItemTemplate = (item) =>
+        <li id={item.data.path}>
+            <a href="#">
+                <span class={item.data.detail.iconClass} />
+                {item.data.detail.label}
+            </a>
+        </li>;
+
+    const onNavItemChanged = (event: ojNavigationList.selectionChanged<string, Route>) => {
+        props.changeRoute(event.detail.value);
+    };
+
+    const getPageElement = (page) => {
+        switch(page) {
+            case "counter":
+                return <Counter />;
+            case "flightbooker":
+                return <FlightBooker />;
+            case "tempconverter":
+                return <TemperatureConverter />;
+            default:
+                return <Counter />;
+        }
+    }
+
+    return (
+        <div class="oj-web-applayout-max-width oj-web-applayout-content">
+            <div class="oj-flex">
+                <div class="oj-flex-item oj-sm-width-1/4">
+                    <oj-navigation-list data={routesDP} onselectionChanged={onNavItemChanged}>
+                        <template slot="itemTemplate" render={navItemTemplate}></template>
+                    </oj-navigation-list>
+                </div>
+                <div class="oj-flex-item oj-sm-width-3/4">
+                    <div class="oj-panel">
+                        {getPageElement(props.page)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
