fbd6625b2dc3ed2ed287fa265b511820adc1f1fa
Zemian Deng <zemian.deng@oracle.com>
2022-06-02 10:59:07 -0400

    Add message banner

--- src/components/gui/FlightBooker.tsx
+++ src/components/gui/FlightBooker.tsx
@@ -5,7 +5,9 @@ import "ojs/ojbutton";
 import "ojs/ojdatetimepicker";
 import "ojs/ojselectsingle";
 import "ojs/ojoption";
+import "ojs/ojmessagebanner";
 import ArrayDataProvider = require("ojs/ojarraydataprovider");
+import MutableArrayDataProvider = require("ojs/ojmutablearraydataprovider");
 
 export function FlightBooker () {
     function pad(n, s = String(n)) {
@@ -33,6 +35,8 @@ export function FlightBooker () {
     const onDepartureDateChanged = (e) => setDepartureDate(e.detail.value);
     const onReturnDateChanged = (e) => setReturnDate(e.detail.value);
 
+    const [messagesDP] = useState(new MutableArrayDataProvider([], {keyAttributes: "id"}));
+
     useEffect(() => {
         const isDisabled = (flightType === 'return' &&
             stringToDate(returnDate) <= stringToDate(departureDate));
@@ -40,14 +44,24 @@ export function FlightBooker () {
     }, [flightType, returnDate, departureDate]);
 
     const book = () => {
-        alert(
-            flightType === 'return'
+        const details = flightType === 'return'
                 ? `You have booked a return flight leaving on ${departureDate} and returning on ${returnDate}.`
                 : `You have booked a one-way flight leaving on ${departureDate}.`
-        );
+        messagesDP.data = [{
+            id: 1,
+            severity: 'confirmation',
+            summary: `Confirmation`,
+            detail: details
+        }];
     }
+
+    const onMessageClose = () => {
+        messagesDP.data = [];
+    }
+
     return (
         <div class="oj-sm-width-1/3">
+            <oj-message-banner data={messagesDP} onojClose={onMessageClose}></oj-message-banner>
             <div>
                 <oj-select-single data={flightTypesDP} value={flightType} onvalueChanged={onFlightTypeChanged}>
                 </oj-select-single>
