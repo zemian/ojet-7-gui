890256d30442d68b68d8c3599ea58678b6c1cb64
Zemian Deng <zemian.deng@oracle.com>
2022-06-02 10:35:41 -0400

    Update event flag to avoid duplicated event handler calls

--- src/components/gui/TemperatureConverter.tsx
+++ src/components/gui/TemperatureConverter.tsx
@@ -3,34 +3,43 @@ import { useState } from "preact/hooks";
 
 import "ojs/ojinputtext";
 
+/*
+ * We are using JET 'event.detail.updatedFrom' to tell the update call to avoid duplicated
+ * invocation where there could be extra call by 'external'.
+ */
 export function TemperatureConverter () {
     const [celsius, setCelsius] = useState(0);
     const [fahrenheit, setFahrenheit] = useState(32);
+
     const onCelsiusChanged = (event: CustomEvent) => {
-        const newValue = event.detail.value;
-        setCelsius(newValue);
-        const newF = newValue * (9 / 5) + 32;
-        setFahrenheit(newF);
+        console.log("onCelsiusChanged", event);
+        if (event.detail.updatedFrom === 'internal') {
+            const newValue = event.detail.value;
+            setCelsius(newValue);
+            const newF = newValue * (9 / 5) + 32;
+            setFahrenheit(newF);
+        }
     }
+
     const onFahrenheitChanged = (event: CustomEvent) => {
-        const newValue = event.detail.value;
-        setFahrenheit(newValue);
-        const newC = (newValue - 32) * (5 / 9);
-        setCelsius(newC);
+        console.log("onFahrenheitChanged", event);
+        if (event.detail.updatedFrom === 'internal') {
+            const newValue = event.detail.value;
+            setFahrenheit(newValue);
+            const newC = (newValue - 32) * (5 / 9);
+            setCelsius(newC);
+        }
     }
-    const CelsiusInputText = () =>
-        <oj-input-text value={celsius} onvalueChanged={onCelsiusChanged}></oj-input-text>;
-    const FahrenheitInputText = () =>
-        <oj-input-text value={fahrenheit} onvalueChanged={onFahrenheitChanged}></oj-input-text>;
+
 
     return (
         <div class="oj-sm-width-1/3">
-            <p>Enter value the press Tab</p>
+            <p>Enter value then press Tab to convert it.</p>
             <p>Celsius:</p>
-            <CelsiusInputText></CelsiusInputText>
+            <oj-input-text value={celsius} onvalueChanged={onCelsiusChanged}></oj-input-text>
 
             <p>Fahrenheit:</p>
-            <FahrenheitInputText></FahrenheitInputText>
+            <oj-input-text value={fahrenheit} onvalueChanged={onFahrenheitChanged}></oj-input-text>
         </div>
     );
 }
