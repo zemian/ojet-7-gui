c1faa80dcc7880cb007046b0c9a68d163b4fbb14
Zemian Deng <zemian.deng@oracle.com>
2022-06-08 10:27:45 -0400

    Add project link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,40 +1,45 @@
-import { h } from "preact";
+import {h} from "preact";
 
 type Props = {
-  links?: FooterLink[]
+    links?: FooterLink[]
 }
 
 type FooterLink = {
-  name: string;
-  linkId: string;
-  linkTarget: string;
+    name: string;
+    linkId: string;
+    linkTarget: string;
 }
 
 const _DEFAULT_LINKS: FooterLink[] = [
-  {
-    name: "7GUIs - A GUI Programming Benchmark",
-    linkId: "7GUIs",
-    linkTarget: "https://eugenkiss.github.io/7guis/"
-  }
+    {
+        name: "Project Source",
+        linkId: "project-source",
+        linkTarget: "https://github.com/zemian/ojet-7-gui/"
+    },
+    {
+        name: "7GUIs - A GUI Programming Benchmark",
+        linkId: "7GUIs",
+        linkTarget: "https://eugenkiss.github.io/7guis/"
+    }
 ]
 
-export function Footer({ links = _DEFAULT_LINKS } : Props ) {
-  return (
-    <footer class="oj-web-applayout-footer" role="contentinfo">
-      <div class="oj-web-applayout-footer-item oj-web-applayout-max-width">
-        <ul>
-          {links.map((item) => (
-            <li>
-              <a id={item.linkId} href={item.linkTarget} target="_blank">
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div class="oj-web-applayout-footer-item oj-web-applayout-max-width oj-text-secondary-color oj-text-sm">
-        A OracleJET implementation of the 7 GUIs for Learning Purpose.
-      </div>
-    </footer>
-  );
+export function Footer({links = _DEFAULT_LINKS}: Props) {
+    return (
+        <footer class="oj-web-applayout-footer" role="contentinfo">
+            <div class="oj-web-applayout-footer-item oj-web-applayout-max-width">
+                <ul>
+                    {links.map((item) => (
+                        <li>
+                            <a id={item.linkId} href={item.linkTarget} target="_blank">
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div class="oj-web-applayout-footer-item oj-web-applayout-max-width oj-text-secondary-color oj-text-sm">
+                A OracleJET implementation of the 7 GUIs for Learning Purpose.
+            </div>
+        </footer>
+    );
 }
